974ffccdf00370d3d75d6df50cb649f34f9316f0
Benjamin Kinzer <ben@benjaminkinzer.com>
2016-01-22 17:17:55 -0800

    Users for features

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121060448) do
+ActiveRecord::Schema.define(version: 20160123010707) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -28,7 +28,10 @@ ActiveRecord::Schema.define(version: 20160121060448) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "features", ["user_id"], name: "index_features_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
